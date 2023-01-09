02f6f0d9ec8da24f73b71c4ef7e39a5f7779623e
Amit Sen <senamit2708@gmail.com>
2023-01-09 15:34:50 +0530

    Initial commit

---
+++ app/src/main/java/ecaps/dna/composeBasicOne/MainActivity.kt
@@ -0,0 +1,23 @@
+package ecaps.dna.composeBasicOne
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ecaps.dna.composeBasicOne.ui.theme.ComposeBasicOneTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text(text = "Good job")
+        }
+    }
+}
+
