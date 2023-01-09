30f96ccb9ebeaa650e3c51dc3bcc05b8795f9608
Amit Sen <senamit2708@gmail.com>
2023-01-09 15:48:38 +0530

    Compose function basic:
    How to create a compose function
    How to create a preview function

--- app/src/main/java/ecaps/dna/composeBasicOne/MainActivity.kt
+++ app/src/main/java/ecaps/dna/composeBasicOne/MainActivity.kt
@@ -3,21 +3,32 @@ package ecaps.dna.composeBasicOne
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import ecaps.dna.composeBasicOne.ui.theme.ComposeBasicOneTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Text(text = "Good job")
+        setContent {//used for creating the layout design
+            MessageCard(message = "Good Morning")//composable function
         }
+    }
+
+    //normal function having composable annotation, makes its composable function.
+    //Composable function is always be called from another composable function or setContent method.
+    //Composable function that returns unit should always start with capital letter.
+    @Composable
+    fun MessageCard(message: String){
+        Text(text = message)
+    }
+
+    //Preview annotation is used to preview the layout design.
+    //preview function doesnt have any parameter, so always wrap parameter function inside a new function having no paramter
+    @Preview
+    @Composable
+    fun PreviewMessageCard(){
+        MessageCard(message = "Good Morning Sunshine")
     }
 }
 
